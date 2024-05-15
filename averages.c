1208462026ded987a08d97593dbd8c775878ff39
Omar27522 <72665821+Omar27522@users.noreply.github.com>
2024-05-14 19:09:10 -0700

    Update
    Find the average of whole numbers.

---
+++ averages.c
@@ -0,0 +1,25 @@
+/*1.4 select the average of numbers. 
+
+Steps:
+Read the total amount of numbers.
+Read (n) numbers
+nums=numbers
+Calculate sum
+sum = (n) +(n)
+Calculate average.
+avg= sum/numb 
+Display average, the average of the total amount of numbers chosen.
+*/
+#include <stdio.h>
+main() 
+	{
+Int
+	total_amount, numbers, sum, avg;
+printf(“Enter the total amount of numbers”);
+scanf(“%d”, &total_amount );
+
+while ( total_amount > 0){
+printf(“Enter number”);
+																				//i need to figure this out
+total_amount++;
+} 
