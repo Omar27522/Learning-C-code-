025990b6940b3cd24288acda85311c97768a21ab
Omar27522 <72665821+Omar27522@users.noreply.github.com>
2024-06-12 13:32:09 -0700

    Update ch1

---
+++ ch1/ three_integers_randomOrder.c
@@ -0,0 +1,20 @@
+/* 1.4.4	Three integers are entered in random order into the computer. 
+
+Your task is to display the numbers in ascending order.
+
+Using the problem-solving methodology, present an algorithm for ordering three numbers.
+
+Your al- gorithm should be presented in pseudocode.
+
+
+
+1.	read 3 integers 
+2.	if firstN > secondN set firstN as bigger, and 			secondN as smaller. else set secondN as 			bigger and firstN as smaller.
+
+3.	if smaller > thirdN set thirdN as smallest.
+		else set smaller as smallest and thirdN as 			smaller.
+		
+4.	if bigger > smaller printf bigger smaller 					smallest. else print smaller bigger smallest. 
+
+I know this pseudo is kinda dopy but I think it works. maybe there is a better approach. 
+*/
